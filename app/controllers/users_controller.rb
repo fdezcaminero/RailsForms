84dee6d77a1c487bff58c288414dc6325f72a29e
elbie-em <emoonga1993@gmail.com>
2020-04-22 22:29:39 +0200

    Added user create logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+
   def create
-    
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
   end
 
   def new
