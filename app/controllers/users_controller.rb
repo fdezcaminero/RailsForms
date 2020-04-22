5488d57210017aae5177fddfab605f57878f3017
fdezcaminero <fdezcaminero@gmail.com>
2020-04-22 17:56:32 -0400

    Updated Form with Form tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
-  #include UsersHelper
+  include UsersHelper
   def create
-    @user = User.new(user_params)
+    #@user = User.new(user_params)
+
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
     if @user.save
       redirect_to new_user_path
@@ -14,6 +16,7 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
