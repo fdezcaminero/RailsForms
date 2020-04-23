d7e564aa27a83d22053b0b385b4a26e7a57414f4
fdezcaminero <fdezcaminero@gmail.com>
2020-04-23 11:47:40 -0400

    Edit is working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,13 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to new_user_path
   end
 
   private
