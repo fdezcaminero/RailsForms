298eb4da268ed9f310f5cf6b8b8f8a57b031a305
fdezcaminero <fdezcaminero@gmail.com>
2020-04-23 11:51:13 -0400

    worked on validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true
+    validates :username, presence: true, uniqueness: true, length: { in: 5..15 }
     validates :email, presence: true
-    validates :password, presence: true
+    validates :password, presence: true, length: { in: 10..15 } 
 end
